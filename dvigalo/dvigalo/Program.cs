82ee12d2473d75a5a4b94b9cc21fb09f71008b7f
Klemen Kosmac <kosmac.stuff@gmail.com>
2024-01-24 10:49:13 +0100

    lolek

---
+++ dvigalo/dvigalo/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dvigalo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("dvigalo se ustavi...");
+            //for(int i=1; i<=10; i++)
+            //{
+            //    Console.WriteLine(i+" nadstropje");
+            //}
+
+            //for(int i=1; i<=10; i = i + 2)
+            //{
+            //    Console.WriteLine(i+" nadstropje");
+            //}
+
+            //Console.WriteLine("ucimo se postevanko");
+            //Console.WriteLine("vnesi karkoli, k=konec");
+            //string vnos = Console.ReadLine();
+            //Random r = new Random();
+            //string konec = Console.ReadLine();
+            //while ( konec.ToUpper() !="k")
+            //{
+            //    int prvo = r.Next(1, 11);
+            //    int drugo = r.Next(1, 11);
+            //    Console.WriteLine(prvo + "x" + drugo+ "=");
+            //    int resitev = int.Parse(Console.ReadLine());
+            //    if(prvo * drugo == resitev)
+            //    {
+            //        Console.WriteLine("pravilno");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("napačno");
+            //    }
+            //    Console.WriteLine("yes?");
+            //}
+
+            //1
+            //int min = 1000000;
+            //for(int i=1; i<=10; i++)
+            //{
+            //    Console.WriteLine("vnesi stevilo");
+            //    int n = int.Parse(Console.ReadLine());
+            //    if (n < min)
+            //    {
+            //        min = n;
+            //    }
+            //}
+            //Console.WriteLine("najmanjse stevilo: "+min);
+
+            //2
+            //Random r = new Random();
+            //int n = r.Next(100);
+            //int y = new int();
+            //for (int i=0; i <= 99; i++)
+            //{
+            //    y = n + n;
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("vsota "+y);
+
+
+            //3
+            for (int i=0; i <= 999; i++)
+            {
+                
+            }
+        }
+    }
+}
---
+++ prog2/prog2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog2
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdravljen git!");
+            int št1 = 15;
+            int št2 = 30;
+            //formatiranje izpisa
+            Console.WriteLine("Moje število 1 je {0,5},drugo število je {1,5}", št1, št2);
+            //celoštevilski formati
+            Console.WriteLine("Decimalno    {0,5:d}", št1);
+            Console.WriteLine("Hexdecimalno {0,5:x}", št1);
+            //realni formati
+            double r = 1234.56789;
+            Console.WriteLine("Realno število z 10:f3= {0,10:f3}", r);
+            Console.WriteLine("Realno število z 10:e3= {0,10:e3}", r);
+            Console.WriteLine("Realno število z 10:p2= {0,10:p2}", r);
+            Console.WriteLine("Realno število z 10:n2= {0,10:n2}", r);
+            Console.WriteLine("Realno število z 10:g2= {0,10:g2}", r);
+            Console.WriteLine("Realno število z 10:c2= {0,10:c2}", r);
+            Console.ReadLine();
+        }
+    }
+}
